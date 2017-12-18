dc3eeb37ecf8ff1c620d477afc974059764bac9a
Rodriguez <DanielRodriguez1@thomsonreuters.com>
2017-12-18 15:37:27 -0600

    Added chapter 5 code

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule, AccordionModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    AccordionModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

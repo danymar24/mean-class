83e106f38d3b879b6c2b3ae348d1903b560f98e3
Rodriguez <DanielRodriguez1@thomsonreuters.com>
2017-12-18 16:09:52 -0600

    Added code for chapter 6

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AlertModule, AccordionModule } from 'ngx-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { ListTodosComponent } from './components/list-todos/list-todos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    ListTodosComponent
   ],
   imports: [
     BrowserModule,

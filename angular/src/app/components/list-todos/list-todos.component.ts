7ae398ee3020a96494c14fac3a527076cf5c380c
Rodriguez <DanielRodriguez1@thomsonreuters.com>
2017-12-20 15:08:00 -0600

    Added code for chapter 9

--- angular/src/app/components/list-todos/list-todos.component.ts
+++ angular/src/app/components/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-todos',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  todos: any;
 
   ngOnInit() {
+    this.http.get('/api/todos').subscribe(res => {
+      this.todos = res;
+      console.log(res);
+    });
   }
 
 }
